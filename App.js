2dae157a668dff9549d10302e51461c493a92361
abj95630 <abah20000@gmail.com>
2021-07-11 14:49:16 +0200

    restructuration du code avec un fichier et utilisation à jour d'EasyBase

--- App.js
+++ App.js
@@ -1,33 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { EasybaseProvider } from "easybase-react";
+import { NativeAuth } from 'easybase-react/native'
+import ebconfig from "./ebconfig"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.entree}>
-        <TextInput style={styles.input} placeholder="Adresse électronique"/>
-        <TextInput style={{ borderWidth: 1, paddingHorizontal: 34 }} placeholder="Mot de passe"/>
-        <Button title="Entrée"/>
-      </View>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <EasybaseProvider ebconfig={ebconfig}>
+      <NativeAuth>
+        <Text>Vous êtes connectés !</Text>
+      </NativeAuth>
+    </EasybaseProvider>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    width: "auto"
-  },
-  entree: {
-    flex: 2,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  input: {
-    marginVertical: 35,
-    borderWidth: 1,
-    paddingHorizontal: 34
-  }
-});
+
--- accueil.js
+++
@@ -1,3 +0,0 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
