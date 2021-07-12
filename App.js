2e5ceffe59817747249dab6255f4d09242f624cc
abj95630 <abah20000@gmail.com>
2021-07-13 01:25:56 +0200

    Ajout du tableau, mise à jour app.json pour apk et insertion de ligne dans la base de données

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, Pressable, StyleSheet, Text, View } from 'react-native';
 import { EasybaseProvider, useEasybase } from "easybase-react";
+import { DataTable } from 'react-native-paper';
 import ebconfig from "./ebconfig";
 import { Connexion } from './connexion';
 
@@ -8,33 +9,78 @@ import { Connexion } from './connexion';
 export default function App() {
   return (
     <EasybaseProvider ebconfig={ebconfig}>
-      <Accueil />
+      <Home />
     </EasybaseProvider>
   );
 }
 
-function Accueil () {
+function Home () {
   const { isUserSignedIn, signOut, db } = useEasybase();
   const [table, setTable] = useState([]);
 
-  {/*useEffect(() => {
-    db("MOT DE PASSE", true).return().limit(20).all()
-    .then(data => setTable(data));
-  }, [])*/}
+  const tablePassword = async() => {
+    const data = await db("MOT DE PASSE").return().limit(10).all();
+    setTable(data);
+  }
+
+  useEffect(() => {
+    tablePassword();
+  }, []);
+
+
+  const createPassword = async() => {
+    try {
+      const websiteInp = Alert.prompt("Le site web");
+      const BackOfficeInp = Alert.prompt("Le back office");
+      const LoginInp = Alert.prompt("Le login");
+      const PasswordInp = Alert.prompt("Le mot de passe");
+      if(!websiteInp || !BackOfficeInp ||!LoginInp || !PasswordInp) return;
+
+      await db("MOT DE PASSE").insert({
+        website: websiteInp,
+        backoffice: BackOfficeInp,
+        login: LoginInp,
+        password: PasswordInp
+      }).one();
+      tablePassword();
+    } catch (_) {
+      alert("Erreur Insertion");
+    }
+  }
 
   return (
     isUserSignedIn() ?
     <View>
-      <Text style={{ marginTop: 30 }}>Bienvenue !</Text>
-      <Button title="Déconnexion" onPress={signOut} />
-      <Text>Voici un tableau !</Text>
-      {table.map(ele => 
-        <Text>{ele.pseudo}</Text>
-      )}
+      <View style={styles.row}>
+        <Button title="Déconnexion" onPress={signOut} />
+      </View>
+      <Text style={{ marginTop: 30 }}>Bienvenue</Text>
+        <DataTable style={{ marginBottom: 45}}>
+          <DataTable.Header>
+            <DataTable.Title>Site Web</DataTable.Title>
+            <DataTable.Title>Back Office</DataTable.Title>
+            <DataTable.Title>Login</DataTable.Title>
+            <DataTable.Title>Mot de passe</DataTable.Title>
+          </DataTable.Header>
+          {table.map(dn => 
+            <DataTable.Row>
+            <DataTable.Cell>{dn.website}</DataTable.Cell>
+            <DataTable.Cell>{dn.backoffice}</DataTable.Cell>
+            <DataTable.Cell>{dn.login}</DataTable.Cell>
+            <DataTable.Cell>{dn.password}</DataTable.Cell>
+          </DataTable.Row>  
+          )}
+        </DataTable>
+      <Button title="Créer un mot de passe" onPress={createPassword} />
     </View>
       :
     <Connexion />
   )
 }
 
-
+const styles = StyleSheet.create({
+  row: {
+    marginTop: 20,
+    alignItems: "flex-end"
+  }
+});
